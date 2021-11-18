1934837182bf1fd1171b2b92c12eaad870b05285
chimneyliner <93776081+chimneyliner@users.noreply.github.com>
2021-11-18 10:58:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+// console.log(document.querySelector(".message").textContent)
+// document.querySelector(".message").textContent = "correct number "
+
+
+// console.log(document.querySelector(".message").textContent)
+// document.querySelector('.number').textContent = 5
+// document.querySelector('.score').textContent = 14
+
+// document.querySelector('.guess').value = 34
+
+// console.log(document.querySelector('.guess').value)
+
+// function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+// }
+// const rndNum = (getRandomIntInclusive(1, 20))
+const rndNum = Math.trunc(Math.random() * 20) + 1
+let score = 20
+
+document.querySelector('.number').textContent = rndNum
+
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value)
+
+
+    if (!guess) {
+        document.querySelector('.message').textContent = "no number slected"
+    } else if (guess == rndNum) {
+        document.querySelector('.message').textContent = "YOU GOT IT"
+
+    } else if (guess > rndNum) {
+        document.querySelector('.message').textContent = "📈 TOO HIGH A GUESS"
+        score = score - 1
+    } else if (guess < rndNum) {
+        document.querySelector('.message').textContent = " 📉 TOO LOW A GUESS"
+        score = score - 1
+    }
+    document.querySelector('.score').textContent = score
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
